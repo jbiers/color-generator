3023948769cbca2f7b97ecbcb9db9905696f3245
jbiers <juliabiersuriano@gmail.com>
2022-04-04 15:41:12 -0400

    added color list structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Header } from "./components/Header"
 import { ColorGenerator } from "./components/ColorGenerator"
 import { GlobalStyle } from "./styles/global"
+import { ColorList } from "./components/ColorList";
 
 export function App() {
   return (
     <>
       <Header />
       <ColorGenerator />
+      <ColorList />
 
       <GlobalStyle />
     </>
---
+++ src/components/ColorList/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export function ColorList() {
+    return (
+        <Container>
+            ColorList
+        </Container>
+    )
+}
