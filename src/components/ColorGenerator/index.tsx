f63092b4248e132842ba2ddf3054a1e752e0b00d
jbiers <juliabiersuriano@gmail.com>
2022-04-04 12:33:51 -0400

    get color input value

--- src/components/ColorGenerator/index.tsx
+++ src/components/ColorGenerator/index.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import { GeneratedColor } from "../GeneratedColor";
 import { GeneratedColorCode } from "../GeneratedColorCode";
 import { Container } from "./styles";
 
 export function ColorGenerator() {
+    const [red, setRed] = useState<Number>(0)
+    const [green, setGreen] = useState<Number>(0)
+    const [blue, setBlue] = useState<Number>(0)
+
     return (
         <Container>
             <div className="colors">
                 <div className="color red">
                     <input type="range" id="red" name="red"
-                        min="0" max="255" />
+                        min="0" max="255" defaultValue="0" onChange={change => setRed(parseInt(change.target.value))} />
                 </div>
                 <div className="color green">
                     <input type="range" id="red" name="red"
-                        min="0" max="255" />
+                        min="0" max="255" defaultValue="0" onChange={change => setGreen(parseInt(change.target.value))} />
                 </div>
                 <div className="color blue">
                     <input type="range" id="red" name="red"
-                        min="0" max="255" />
+                        min="0" max="255" defaultValue="0" onChange={change => setBlue(parseInt(change.target.value))} />
                 </div>
             </div>
 
